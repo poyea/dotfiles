00a6f275f51dbf5f58f0652b1d16fccfeaa89385
John Law <johnlaw.po@gmail.com>
2022-04-11 10:38:09 +0000

    fix: link all files

--- setup.sh
+++ setup.sh
@@ -4,13 +4,10 @@ DIR=$(cd $(dirname $0) ; pwd)
 
 setup_ubuntu()
 {
-    ln -si $DIR/ubuntu/.bashrc ~/.bashrc
-    ln -si $DIR/ubuntu/.bash_aliases ~/.bash_aliases
-    ln -si $DIR/ubuntu/.bashrc.local ~/.bashrc.local
-    ln -si $DIR/ubuntu/.bash_funcs.sh ~/.bash_funcs.sh
-
-    ln -si $DIR/ubuntu/.tmux.conf ~/.tmux.conf
-    ln -si $DIR/ubuntu/.vimrc ~/.vimrc
+    for file in $DIR/ubuntu/.*; do
+      [ -d "$file" ] && continue
+      ln -si $file ~/$(basename $file)
+    done
 }
 
 select i in ubuntu exit
