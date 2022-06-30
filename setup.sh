04cabcf4f051d1fe9c7536fc9db9afb282c6ed9e
John Law <johnlaw.po@gmail.com>
2022-07-01 02:29:17 +0800

    feat: add command check

--- setup.sh
+++ setup.sh
@@ -4,19 +4,37 @@ DIR=$(cd $(dirname $0) ; pwd)
 
 setup_ubuntu()
 {
-    for file in $DIR/ubuntu/.*; do
-      [ -d "$file" ] && continue
-      ln -si $file ~/$(basename $file)
-    done
+  for file in $DIR/ubuntu/.*; do
+    [ -d "$file" ] && continue
+    ln -si $file ~/$(basename $file)
+  done
 }
 
-select i in ubuntu exit
+exists()
+{
+  command -v $1 1>/dev/null 2>&1 || return 1
+  return 0
+}
+
+check_utils()
+{
+  for cmd in bat vim htop
+  do
+    if ! exists $cmd; then echo "Cannot find $cmd"; fi
+  done
+}
+
+select i in ubuntu check exit
 do
   case $i in
     ubuntu)
-        setup_ubuntu
-        exit
-        ;;
+      setup_ubuntu
+      exit
+      ;;
+    check)
+      check_utils
+      exit
+      ;;
     exit) exit;;
   esac
 done
